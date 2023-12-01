c88c9ea57ebbcfa15206b011b3518464ebd81165
xticion <xticion@gmail.com>
2023-12-01 09:34:49 +0800

    uniapp demo 初始化提交

---
+++ src/pages/my/my.vue
@@ -0,0 +1,9 @@
+<template>
+  <view class="my">my</view>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style lang="less" scoped></style>
---
+++ src/stores/modules/member.ts
@@ -0,0 +1,35 @@
+import { ref, computed } from 'vue'
+import { defineStore } from 'pinia'
+
+export const userMemberStore = defineStore(
+    'member',
+    ()=>{
+        const profile = ref();
+
+        const serProfile = (val:any) =>{
+            profile.value = val
+        }
+
+        const clearProfile = () =>{
+            profile.value = undefined
+        }
+
+        return {
+            profile,
+            serProfile,
+            clearProfile
+        }
+    },
+    { 
+        persist:{
+            storage:{
+                getItem(key){
+                    return uni.getStorageSync(key);
+                },
+                setItem(key,value){
+                    return uni.setStorageSync(key,value);
+                }
+            }
+        }
+    }
+)
